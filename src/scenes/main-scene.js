f0ce44011bcd6cdeff3d8ca73d9d0d951a8b7713
Megabyte <vasiliy.p.kostin@gmail.com>
2018-07-03 18:33:55 +0300

    linebreaks convert

--- src/scenes/main-scene.js
+++ src/scenes/main-scene.js
@@ -1,5 +1,4 @@
 import Scene from "/thing-engine/js/components/scene.js";
-import TilemapProcessor from "../utils/tilemap-processor.js"; // eslint-disable-line no-unused-vars
 
 export default class MainMenu extends Scene {
 	
