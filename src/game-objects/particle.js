9f039115b93adfb95d1ae7a087f7695f75822208
vasiliyk <vasiliyk@playtika.com>
2018-07-13 12:14:57 +0300

    particle added

---
+++ src/game-objects/particle.js
@@ -0,0 +1,34 @@
+import DSprite from "/thing-engine/js/components/d-sprite.js";
+
+export default class Particle extends DSprite {
+
+	init() {
+		/// #if EDITOR
+		__getNodeExtendData(this).hidden = true;
+		/// #endif
+		super.init();
+		this.rSpeed = Math.random() - 0.5;
+		this.life = 2.0;
+		this.scl = Math.random()* 0.6 + 0.2;
+		this.scale.x = this.scl;
+		this.scale.y = this.scl;
+		this.alpha = Math.random()* 0.6 + 0.2;
+	}
+
+	update() {
+		this.xSpeed += (Math.random() - 0.5) * 0.3;
+		this.ySpeed += (Math.random() - 0.6) * 0.3;
+		this.xSpeed *= 0.95;
+		this.ySpeed *= 0.95;
+		super.update();
+		this.life -= 0.1;
+		if(this.life < 0.001) {
+			this.remove();
+		} else {
+			if(this.life <= 1) {
+				this.scale.x = this.scl * this.life;
+				this.scale.y = this.scl * this.life;
+			}
+		}
+	}
+}
