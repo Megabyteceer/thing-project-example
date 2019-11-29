6bc592d97565c802d0b2c05ee0b784625537e996
Megabyte <vasiliy.p.kostin@gmail.com>
2019-11-29 18:06:53 +0300

    pixi5 progress

--- src/game-objects/perspective-sprite.js
+++ src/game-objects/perspective-sprite.js
@@ -1,65 +1,65 @@
 import Lib from "thing-engine/js/lib.js";
 
-class PerspectiveSprite extends PIXI.mesh.Plane {
+class PerspectiveSprite extends PIXI.SimplePlane {
 	
 	constructor() {
 		super(Lib.getTexture('WHITE'), 2 , 2);
-		//this.vertices = [1,2,3,4,5,6,7,8];
+		//this.vertexData = [1,2,3,4,5,6,7,8];
 		//this.uvs = [0,0,1,0,1,1,1,0];
 	}
 	
 	update() {
-		this.vertices[0] = Math.random() * -20;
-		this.vertices[1] = Math.random() * 20;
+		this.vertexData[0] = Math.random() * -20;
+		this.vertexData[1] = Math.random() * 20;
 	}
 	
 	get x0() {
-		return this.vertices[0];
+		return this.vertexData[0];
 	}
 	set x0(v) {
-		this.vertices[0] = v;
+		this.vertexData[0] = v;
 	}
 	get y0() {
-		return this.vertices[1];
+		return this.vertexData[1];
 	}
 	set y0(v) {
-		this.vertices[1] = v;
+		this.vertexData[1] = v;
 	}
 	get x1() {
-		return this.vertices[2];
+		return this.vertexData[2];
 	}
 	set x1(v) {
-		this.vertices[2] = v;
+		this.vertexData[2] = v;
 	}
 	get y1() {
-		return this.vertices[3];
+		return this.vertexData[3];
 	}
 	set y1(v) {
-		this.vertices[3] = v;
+		this.vertexData[3] = v;
 	}
 	get x2() {
-		return this.vertices[4];
+		return this.vertexData[4];
 	}
 	set x2(v) {
-		this.vertices[4] = v;
+		this.vertexData[4] = v;
 	}
 	get y2() {
-		return this.vertices[5];
+		return this.vertexData[5];
 	}
 	set y2(v) {
-		this.vertices[5] = v;
+		this.vertexData[5] = v;
 	}
 	get x3() {
-		return this.vertices[6];
+		return this.vertexData[6];
 	}
 	set x3(v) {
-		this.vertices[6] = v;
+		this.vertexData[6] = v;
 	}
 	get y3() {
-		return this.vertices[7];
+		return this.vertexData[7];
 	}
 	set y3(v) {
-		this.vertices[7] = v;
+		this.vertexData[7] = v;
 	}
 }
 
