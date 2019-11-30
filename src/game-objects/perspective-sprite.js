5830345f430800a47631dfd41330d603577db7f8
Megabyte <vasiliy.p.kostin@gmail.com>
2019-11-30 12:25:37 +0300

    pixi 5 fixes. WHITE teture 16x16 size

--- src/game-objects/perspective-sprite.js
+++ src/game-objects/perspective-sprite.js
@@ -4,62 +4,68 @@ class PerspectiveSprite extends PIXI.SimplePlane {
 	
 	constructor() {
 		super(Lib.getTexture('WHITE'), 2 , 2);
-		//this.vertexData = [1,2,3,4,5,6,7,8];
-		//this.uvs = [0,0,1,0,1,1,1,0];
 	}
 	
 	update() {
-		this.vertexData[0] = Math.random() * -20;
-		this.vertexData[1] = Math.random() * 20;
+		this.x0 = Math.random() * -20;
+		this.y0 = Math.random() * 20;
 	}
 	
 	get x0() {
-		return this.vertexData[0];
+		return this.verticesBuffer.data[0];
 	}
 	set x0(v) {
-		this.vertexData[0] = v;
+		this.verticesBuffer.data[0] = v;
+		this.verticesBuffer._updateID++;
 	}
 	get y0() {
-		return this.vertexData[1];
+		return this.verticesBuffer.data[1];
 	}
 	set y0(v) {
-		this.vertexData[1] = v;
+		this.verticesBuffer.data[1] = v;
+		this.verticesBuffer._updateID++;
 	}
 	get x1() {
-		return this.vertexData[2];
+		return this.verticesBuffer.data[2];
 	}
 	set x1(v) {
-		this.vertexData[2] = v;
+		this.verticesBuffer.data[2] = v;
+		this.verticesBuffer._updateID++;
 	}
 	get y1() {
-		return this.vertexData[3];
+		return this.verticesBuffer.data[3];
 	}
 	set y1(v) {
-		this.vertexData[3] = v;
+		this.verticesBuffer.data[3] = v;
+		this.verticesBuffer._updateID++;
 	}
 	get x2() {
-		return this.vertexData[4];
+		return this.verticesBuffer.data[4];
 	}
 	set x2(v) {
-		this.vertexData[4] = v;
+		this.verticesBuffer.data[4] = v;
+		this.verticesBuffer._updateID++;
 	}
 	get y2() {
-		return this.vertexData[5];
+		return this.verticesBuffer.data[5];
 	}
 	set y2(v) {
-		this.vertexData[5] = v;
+		this.verticesBuffer.data[5] = v;
+		this.verticesBuffer._updateID++;
 	}
 	get x3() {
-		return this.vertexData[6];
+		return this.verticesBuffer.data[6];
 	}
 	set x3(v) {
-		this.vertexData[6] = v;
+		this.verticesBuffer.data[6] = v;
+		this.verticesBuffer._updateID++;
 	}
 	get y3() {
-		return this.vertexData[7];
+		return this.verticesBuffer.data[7];
 	}
 	set y3(v) {
-		this.vertexData[7] = v;
+		this.verticesBuffer.data[7] = v;
+		this.verticesBuffer._updateID++;
 	}
 }
 
